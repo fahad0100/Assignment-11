6d2aae3c87b5d50401b92fc316c485daed6157a5
fahad0100 <fahad.alazmi1994@gmail.com>
2025-04-21 11:50:29 +0300

    asdfas

---
+++ dddd/bin/dddd.dart
@@ -0,0 +1,5 @@
+import 'package:dddd/dddd.dart' as dddd;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dddd.calculate()}!');
+}
---
+++ dddd/test/dddd_test.dart
@@ -0,0 +1,8 @@
+import 'package:dddd/dddd.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
